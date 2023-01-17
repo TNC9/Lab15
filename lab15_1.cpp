32380a2aa7103d0743f41b62abc0ea4311177658
TNC9 <koonbenzkub@hotmail.com>
2023-01-17 15:57:13 +0700

    Complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for (int i = 1; i <= N-1; i++){
+		T temp = d[i];
+		int x = i-1;
 
+		while (x >= 0 && temp > d[x]){
+            d[x+1] = d[x];
+            x = x - 1;
+        }
+        d[x + 1] = temp;
+		cout << "Pass " << i << ":";
+
+		for (int i = 0; i < N; i++){
+			cout << d[i] << " ";
+		}
+		cout << endl;
+	}
 }
 
 int main(){
