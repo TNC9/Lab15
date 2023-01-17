313fb260420404ced24e069458e7a1c3bd66a87b
TNC9 <koonbenzkub@hotmail.com>
2023-01-17 16:56:20 +0700

    Complete

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,36 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double IM[][N]){
+	for (int i = 0; i < N; i++){
+	    cout << "Row " << i+1 << ":";
+		for (int j = 0; j < N; j++){
+			cin >> IM[i][j];
+		}
+		cout << " ";
+	}
+}
+
+void findLocalMax(const double x[][N],bool y[][N]){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(i == 0 || j == 0 || i == N-1 || j == N-1){
+				y[i][j]= 0;
+			}else if(x[i][j]>=x[i-1][j] && x[i][j]>=x[i+1][j] && x[i][j]>=x[i][j-1] && x[i][j] >= x[i][j+1]){
+				y[i][j]= 1;
+			}else{
+				y[i][j]= 0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool SM[][N]){
+	for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << SM[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
